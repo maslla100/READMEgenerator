00afe7fa406e0a3eea484e1bdf7069e3101c4216
luis llamas <luis.llamas@maslla.com>
2024-01-07 14:51:13 -0600

    modiefied index.js again

--- Develop/index.js
+++ Develop/index.js
@@ -12,6 +12,52 @@ const questions = [
         name: 'title',
         message: 'What is the title of your project?'
     },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use:'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None']
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'If your project has a lot of features, list them here:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples on how to run tests for your application:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address for the Questions section:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username for the Questions section:'
+    }
     // Add other questions here for description, installation, etc.
     // Refer to the acceptance criteria for all required questions.
 ];
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,6 +14,61 @@ function renderLicenseSection(license) {
 
 function generateMarkdown(data) {
     return `# ${data.title}
+    ## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Installation
+- ${data.installation}
+
+
+## Usage
+- ${data.usage}
+
+
+## Credits
+- ${data.credits}
+
+
+## License
+${renderLicenseSection(data.license)}
+- [License]${renderLicenseLink(data.license)}
+
+
+## Badges
+- [License Badge]${renderLicenseBadge(data.license)}
+
+
+
+## User input for Features
+- ${data.features}
+
+
+## User input for Tests
+- ${data.tests}
+
+
+## Questions
+- [Email]${data.email}
+    - [GitHub]${data.github}
+
+
+
+
+
+---
+
 
 // Add other sections like Description, Installation, etc., based on user input.
 
